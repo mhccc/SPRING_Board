b909f9f6252a5ecdb66103d86e03e3c75e77e977
mhccc <mhccc@naver.com>
2018-05-24 17:28:34 +0900

    Add AoP logging

---
+++ src/main/java/org/mccc/springboard/aop/LogAdvice.java
@@ -0,0 +1,53 @@
+package org.mccc.springboard.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+public class LogAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogAdvice.class);
+
+    @Around("execution(* org.mccc.springboard.controller.*Controller.*(..))"
+            + " or execution(* org.mccc.springboard.service.*Impl.*(..))"
+            + " or execution(* org.mccc.springboard.persistence.*Impl.*(..))")
+    public Object logPrint(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        Object result = proceedingJoinPoint.proceed();
+
+        String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String name = "";
+
+        if (type.contains("Controller")) {
+            name = "Controller : ";
+        } else if (type.contains("Service")) {
+            name = "Service : ";
+        } else if (type.contains("DAO")) {
+            name = "Persistence : ";
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        logger.info(name + type + "." + proceedingJoinPoint.getSignature().getName() + "()");
+        logger.info("Argument/Parameter : " + Arrays.toString(proceedingJoinPoint.getArgs()));
+        if (result != null) {
+            logger.info("Return Value : " + result.toString());
+        } else {
+            logger.info("Return Type : void");
+        }
+        logger.info("Running Time : " + (endTime-startTime));
+        logger.info("----------------------------------------------------------------");
+
+        return result;
+    }
+
+}
